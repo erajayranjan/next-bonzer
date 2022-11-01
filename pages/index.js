d69fb8c05fb2d3f3b6e9012fc83637decd2ba159
Ajay Ranjan <ajayranjan@live.com>
2022-11-01 23:39:14 +0530

    Changed

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to BonzerCode Internship on  <a href="https://nextjs.org">Next.js!</a>
+          BonzerCode Internship on  <a href="https://nextjs.org">Next.js!</a>
         </h1>
       </main>
 
