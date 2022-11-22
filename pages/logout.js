f59e30d4d1e7fea82dbe8848626aa2b72a98912b
Ajay Ranjan <ajayranjan@live.com>
2022-11-22 23:46:51 +0530

    Added Logout

--- pages/logout.js
+++ pages/logout.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Router from 'next/router'
 
 const logout = () => {
+
+  useEffect(() => {
+    localStorage.removeItem("bonzerCode") && alert("Successfully logged out")
+  }, [])
+  
+  // Router.reload(window.location.pathname)
+
   return (
     <div>logout</div>
   )
